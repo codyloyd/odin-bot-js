571cf5a2fe588301b8b5241cdc7947156d2309e5
Cody Loyd <codyloyd@gmail.com>
2016-09-15 13:03:52 -0500

    added bot token

--- config.js
+++ config.js
@@ -3,34 +3,38 @@ var config = {
   // gitter settings for retrieving and sending messages
   gitter: {
     // gitter token, can be retrieved from https://developer.gitter.im/apps
-    token: '122496bd738d5708a217116f65a12a3eb086ca1a',
+    token: process.env.GITTER_TOKEN,
 
 
     room: {
       // name of the gitter room, you want to retrieve and send the messages
       name: 'TheOdinProject/bot-spam-playground'
     },
-    
+
     // custom webhook to send the gifs to the activity feed
     webhook: 'https://webhooks.gitter.im/e/ca75b94537a0fb91d19f',
-    
+
     // place, where gif should be sent
     // possibilities: activity, chat
     // if "chat" the message will be send from the user from whom the token is
     place: 'chat'
 
   },
-  
+
   giphy: {
-   
+
    // giphy api key
-   apikey: 'dc6zaTOxFJmzC',
-   
+   apikey: process.env.GIPHY_API_KEY,
+
    // regex to match the giphy command (could be anything)
    regex: /^\/giphy/
-    
+  },
+
+  pointsbot: {
+    token: process.env.POINTSBOT_TOKEN,
+    regex: /@\S+\s?\+\+/
   }
-  
+
 }
 
 module.exports = config;
