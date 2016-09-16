96d372a3ff5acf11db34149e04cc39e50faf21b0
Cody Loyd <codyloyd@gmail.com>
2016-09-15 22:32:55 -0500

    pasta

--- index.js
+++ index.js
@@ -11,102 +11,77 @@ var giphy = new Giphy(config.giphy.apikey);
 
 // gitter room name from config gets joined, to receive the room id on start
 gitter.rooms.join( config.gitter.room.name , function(err, room) {
-  
   if (err) {
     console.log('Not possible to join the room: ', err);
     return;
   }
-  
   config.gitter.room.id = room.id;
-  
   // start the message listener
   listenToMessages();
-  
 })
 gitter.rooms.join( "TheOdinProject/Random" , function(err, room) {
-  
   if (err) {
     console.log('Not possible to join the room: ', err);
     return;
   }
-  
   config.gitter.room.id = room.id;
-  
   // start the message listener
   listenToMessages();
-  
 })
 
 function listenToMessages () {
-
   gitter.rooms.find(config.gitter.room.id).then(function(room) {
- 
     var events = room.streaming().chatMessages();
-   
     // The 'snapshot' event is emitted once, with the last messages in the room 
     events.on('snapshot', function(snapshot) {
       console.log(snapshot.length + ' messages in the snapshot');
     });
-   
     // event gets called, when a new message gets written in the configured channel
     events.on('chatMessages', function(message) {
-      
       // the bot only evaluates new messages, no updates or other changes
       if (message.operation === 'create') {
-        
         var data = message.model;
         var text = data.text;
-        
         // text contains the giphy command
         if (text.match(config.giphy.regex)) {
-          
           var user = data.fromUser.username;
           var search = text.replace(config.giphy.regex, '');
-          
           // replace underscores and colons to spaces because emojis
           search = search.replace(/_|:/g, ' ').trim();
-          
           // if there is search text, search after it
           if (search) {
-          
+            if (parseInt(Math.random()*20) == 0) {
+              search = "pasta"
+            }
             giphy.search({q: search, limit: 20}, function (err, result, res) {
-              
               // if there are results, send a random one
               if (result.data.length) {
-              
                 var image = result.data[parseInt(Math.random()*result.data.length)];
-                
                 var imgurl = image.images.original.url;
-                
                 var feedcontent =  '@' + user + ' : __'+ search +'__ \n\n[!['+search+'](' + imgurl + ')](' + image.url + ')';
-
-                
                 send(feedcontent, room);
-                
+                if (search == "pasta") {
+                  setTimeout(function(){
+                    send("PAAASTAAA :trollface:", room)
+                  }, 6000)
+                }
               } else {
-                
                 // otherwise send a message, that there are no gifs with that search
                 var help = '@' + user + ': there is no gif with those words';
-                
                 send(help, room);
-                
               }
               
             });
-            
           } else {
-            
             // otherwise send an explanation to user
             var help = '@' + user + ': use `/giphy` with a word, to get a gif related to that word, eg. `/giphy cats hats`';
-            
             send(help, room);
-            
           }
-          
+        } else if (text.toLowerCase().match("hello odin-bot")) {
+          var user = data.fromUser.username
+          send("Hello " + "@" + user, room)
         }
-        
       }
-      
     });
   });
 
