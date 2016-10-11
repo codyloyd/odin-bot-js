bbd1f0540abbc6fe4846c77a66dc59accd2bc783
Cody Loyd <codyloyd@gmail.com>
2016-10-11 10:26:56 -0500

    refactoring

--- config.js
+++ config.js
@@ -6,12 +6,27 @@ var config = {
     token: 'a0db2d4e6004bb4aeaadb0381148bc9a4aba8aed',
 
 
-    room: {
-      // name of the gitter room, you want to retrieve and send the messages
-      name: 'TheOdinProject/bot-spam-playground'
-    },
+    // room: {
+    //   // name of the gitter room, you want to retrieve and send the messages
+    //   name: 'TheOdinProject/bot-spam-playground'
+    // },
 
-    rooms: ["Random","bot-spam-playground", "theodinproject","Ruby","Rails","HTML-CSS","Javascript","Git","Getting-Hired","Contributing-Activity","Code-Review","Contributing"],
+    rooms: [
+      "bot-spam-playground",
+      "Code-Review",
+      "Computer-Science",
+      "Contributing",
+      "ContributingActivtity",
+      "Getting-Hired",
+      "Git",
+      "HTML-CSS",
+      "Interesting-Stuff",
+      "Javascript",
+      "Rails",
+      "Random",
+      "Ruby",
+      "theodinproject"
+    ],
 
     // custom webhook to send the gifs to the activity feed
     webhook: 'https://webhooks.gitter.im/e/ca75b94537a0fb91d19f',
