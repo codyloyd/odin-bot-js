b5c943421cc88a8188f0e260ff65c6f5f71a7779
root <codyloyd@gmail.com>
2016-11-15 21:26:23 +0000

    giphy error resolved

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function chooseRandomGif(searchTerm) {
     giphy.search({q: searchTerm, limit:10}, function(err, result) {
       if (err) reject('Error');
 
-      if (result) {
+      if (result.data.length) {
         var image = result.data[randomInt(result.data.length)];
         var imageUrl = image.images.original.url;
         resolve(imageUrl);
